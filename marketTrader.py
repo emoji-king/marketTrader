1a37bf93e39e8635231db5e9fac05ad6d6185b55
jacksonhorton <jacksonhorton1212@gmail.com>
2019-12-25 21:25:00 -0600

    Minor bug fixes and added MacOS support

--- marketTrader.py
+++ marketTrader.py
@@ -1,4 +1,4 @@
-# marketTrader.py v1.0
+# marketTrader.py v1.1
 # WIP; still using paper trades. Also, I apologise for some sloppiness down in the code.
 import config
 import json
@@ -11,6 +11,7 @@ import xlwings.constants
 from bs4 import BeautifulSoup
 from datetime import datetime, time as Time
 from holidays import US as holidaysUS
+from os import listdir
 from pyautogui import alert
 from pytz import timezone
 from sklearn.neural_network import MLPRegressor
@@ -26,9 +27,16 @@ If notify is True, it will notify you when a trade is made;
 If check_close is true, the program will close itself if the market is closed(if False, may not work correctly!);
 Make sure you have a config.py file in the local directory that contains an API key(ex in README)
 '''
-num_of_stocks = 10
+num_of_stocks = 5
 notify = True
-check_close = False
+check_close = True
+
+
+def clean():
+    print(':Deleted old .xlsx files.')
+    for file_name in listdir(os.path.dirname(os.path.abspath(__file__))):
+        if file_name.endswith('.xlsx'):
+            os.remove(os.path.dirname(os.path.abspath(__file__)) + '/' + file_name)
 
 
 def afterHours(now=None):
@@ -54,42 +62,34 @@ def getTopStocks():
     global stock_list
     data = requests.get('https://finance.yahoo.com/screener/predefined/day_gainers')
     if data.status_code != 200:
-        print(f'Error! Status code was {data.status_code}.')
+        print('Error! Status code was {}.'.format(data.status_code))
     html_data = BeautifulSoup(data.content , 'html.parser')
     temp = list(html_data.children)[1]
     temp = list(temp.children)[1]
     temp = list(temp.children)[2]
     html = str(list(temp.children)[0])
     stock_list = html[html.find('"pageCategory":"YFINANCE:') + 25:html.find('","fallbackCategory":')].split(',')[0:num_of_stocks]   #list indexed starts with meta data object, so add 1 to index because index[0] is removed
-    print(f':Top {len(stock_list)} stocks: {stock_list}')
+    print(':Top {} stocks: {}.'.format(num_of_stocks, stock_list))
 
 
 def getStockData(stock):
     global stock_list
     if afterHours() is True and check_close is True:
+        print(':The market is closed...')
         return False
-    if stock is 'All':
+    if stock == 'All':
         stock_list = stock_list
     elif type(stock) is list:
         stock_list = stock
     else:
         stock_list = []
         stock_list.append(stock)
-    for stock in stock_list:
-        try:
-            resp = requests.get('https://www.alphavantage.co/query?', {'function':'TIME_SERIES_INTRADAY','symbol':stock,'interval': '1min', 'apikey': config.API_KEY})
-            if 'Error Message' in resp.text:
-                print(f':Removed {stock} from list.')
-                stock_list.remove(stock)
-        except:
-            stock_list.remove(stock)
-        time.sleep(5)
-    # stores data for each stock in an excel file
     stockDict = {}
     for symbol in stock_list:
-        if os.path.exists(f'{symbol}.xlsx'):
-            os.remove(f'{symbol}.xlsx')
-        with open(f'{symbol}.xlsx', 'w') as w:
+        if os.path.exists('{}.xlsx'.format(symbol)):
+            os.remove('{}.xlsx'.format(symbol))
+        time.sleep(3)
+        with open('{}.xlsx'.format(symbol), 'w') as w:
             w.write('')
         # gets stock data
         data = {
@@ -100,20 +100,28 @@ def getStockData(stock):
             'datatype': 'json' ,
             'apikey': config.API_KEY
         }
-        response = requests.get('https://www.alphavantage.co/query?', params=data)
+        try:
+            response = requests.get('https://www.alphavantage.co/query?', params=data)
+            if 'Error Message' in response.text:
+                print(':Removed {} from list.'.format(symbol))
+                stock_list.remove(stock)
+                pass
+        except:
+            stock_list.remove(stock)
+            pass
         try:
             dummy = response.json()['Time Series (1min)']
         except Exception as exc:
-            print(f':While getting stock data for {symbol}, got error from AlphaVantage... Waiting 15 seconds...')
+            print(':While getting stock data for {}, got error from AlphaVantage... Waiting 15 seconds...'.format(symbol))
             with open('error.txt', 'a') as a:
-                a.write(f'Error: {exc}\njson data: {response.json()}')
+                a.write('Error: {}\njson data: {}'.format(exc, response.json))
             time.sleep(15)
             getStockData(stock_list[stock_list.index(symbol): len(stock_list)])
             break
         data = response.json()['Time Series (1min)']
         # transfers data to excel
         stock_finances = []
-        workbook = xlsxwriter.Workbook(f'{symbol}.xlsx')
+        workbook = xlsxwriter.Workbook('{}.xlsx'.format(symbol))
         worksheet = workbook.add_worksheet('stock data')
         worksheet.write(0 , 1 , "open")
         worksheet.write(0 , 2 , 'high')
@@ -138,35 +146,35 @@ def getStockData(stock):
         # avg per day
         worksheet.write(0 , 7 , 'Day Avg.')
         while i < maxRow:
-            worksheet.write(i , 7 , f'=(B{i + 1}+E{i + 1})/2' , num_format)
+            worksheet.write(i , 7 , '=(B{}+E{})/2'.format(i+1, i+1) , num_format)
             i += 1
         worksheet.write(maxRow - 2 , 8 , 'Avg(Total)')
-        worksheet.write(maxRow - 1 , 8 , f'=AVERAGE(H2:H{maxRow})' , num_format)
+        worksheet.write(maxRow - 1 , 8 , '=AVERAGE(H2:H{})'.format(maxRow) , num_format)
         # total percent change
         worksheet.write(maxRow - 2 , 10 , 'Total %change')
-        worksheet.write(maxRow - 1 , 10 , f'=((E2-B101)/B101)*100' , num_format)
+        worksheet.write(maxRow - 1 , 10 , '=((E2-B101)/B101)*100', num_format)
         workbook.close()
         # gets formula results to compile a dict
-        wb = xw.Book(f'{symbol}.xlsx')
-        percentChange = wb.sheets['stock data'].range(f'K{maxRow}').value
-        totalAvg = wb.sheets['stock data'].range(f'I{maxRow}').value
+        wb = xw.Book('{}.xlsx'.format(symbol))
+        percentChange = wb.sheets['stock data'].range('K{}'.format(maxRow)).value
+        totalAvg = wb.sheets['stock data'].range('I{}'.format(maxRow)).value
         wb.app.quit()
-        stockDict[symbol] = f'[{percentChange}, {totalAvg}]'
+        stockDict[symbol] = '[{}, {}]'.format(percentChange, totalAvg)
         with open("stockData.json" , "a") as append:
             json.dump(stockDict , append)
         # displays formula results
         if percentChange != None:
-            print(f':{symbol} total change is {str(percentChange)[0:str(percentChange).find(".") + 2]}%')
+            print(':{} total change is {}%'.format(symbol, str(percentChange)[0:str(percentChange).find(".") + 2]))
         else:
-            print(f':{symbol} encountered an error during parsing... Removing from list.')
-            if os.path.exists(f'{symbol}.xlsx'):
-                os.remove(f'{symbol}.xlsx')
+            print(':{} encountered an error during parsing... Removing from list.'.format(symbol))
+            if os.path.exists('{}.xlsx'):
+                os.remove('{}.xlsx'.format(symbol))
             stock_list.remove(symbol)
 
 
 def neuralNetPredition(symbol):
-    print(f':Starting neural network training for {symbol}...')
-    wb = xw.Book(f'{symbol}.xlsx')
+    print(':Starting neural network training for {}...'.format(symbol))
+    wb = xw.Book('{}.xlsx'.format(symbol))
     # gets value of maxRow
     RR2 = wb.sheets['stock data'].api.Cells.Find(What="*" ,
                                                  After=wb.sheets['stock data'].api.Cells(1 , 1) ,
@@ -179,26 +187,25 @@ def neuralNetPredition(symbol):
     if maxRow << 500:
         pass
     x_list , y_list = [] , []
-    row = 3
-    list = wb.sheets['stock data'].range(f'B{row}:E{row}').value
+    row = 3 #default is 3; you can increase for testing
+    list = wb.sheets['stock data'].range('B{}:E{}'.format(row, row)).value
     cellRange = range(row , maxRow)
     # creates x and y data sets
     for row in cellRange:
-        y = wb.sheets['stock data'].range(f'E{row - 1}').value
-        values = wb.sheets['stock data'].range(f'B{row}:E{row}').value
+        y = wb.sheets['stock data'].range('E{}'.format(row - 1)).value
+        values = wb.sheets['stock data'].range('B{}:E{}'.format(row, row)).value
         x_list.append(values)
         y_list.append(y)
     # runs neural network model
-    row_to_predict = 2
-    cells = f'B{row_to_predict}:E{row_to_predict}'
+    row_to_predict = row-1   #2
+    cells = 'B{}:E{}'.format(row_to_predict, row_to_predict)
     clf = MLPRegressor(solver='lbfgs' , alpha=1e-5 , random_state=1)
     clf.fit(x_list , y_list)
     guess = clf.predict([wb.sheets['stock data'].range(cells).value])
-    print(f':Model prediciton for {symbol}: {guess}')
+    print(':Model prediciton for {}: {}'.format(symbol, guess))
     with open('log.txt' , 'a') as aF:
-        aF.write(
-            f":{symbol}'s machine learning data: guess={guess};\npredicitonX_value={wb.sheets['stock data'].range(cells).value};\nmaxRows={maxRow};\n\n")
-    prevClose = wb.sheets['stock data'].range(f'E{row_to_predict}').value
+        aF.write(":{}'s machine learning data: guess={};\npredicitonX_value={};\nmaxRows={};\n\n".format(symbol, guess, wb.sheets['stock data'].range(cells).value), maxRow)
+    prevClose = wb.sheets['stock data'].range('E{}'.format(row_to_predict)).value
     percentChange = ((guess-prevClose)/prevClose)*100
     wb.app.quit()
     resultList = [float(guess), float(percentChange), prevClose]
@@ -208,8 +215,9 @@ def neuralNetPredition(symbol):
 def activeTrader(symbol):
     global stock_list
     global prediciton
-    print(f'[activeTrader]: Initializing stock {symbol}')
+    print('[activeTrader]: Initializing stock {}'.format(symbol))
     investment = False
+    investPrice = None
     while True:
         if getStockData([symbol]) is False and check_close is True:
             print(':The market is closed...')
@@ -218,34 +226,35 @@ def activeTrader(symbol):
         cur_price = si.get_live_price(symbol)
         #should i invest
         if investment is False:
-            if prediction[1] > 0.021:
-                print(f'[activeTrader]: Investing in {symbol}; estimated gain is {prediction[1]}; entry price is {cur_price}.')
+            if prediction[1] > 0.021:      # 0.021
+                print('[activeTrader]: Investing in {}; estimated gain is {}%; entry price is {}.'.format(symbol, prediction[1], cur_price))
                 with open('trade_log.txt', 'a') as w:
-                    w.write(f'Invested at {cur_price}. Predicitons: {prediction};\n')
+                    w.write('Invested at {}. Predicitons: {};\n'.format(cur_price, prediciton))
+                investPrice = cur_price
                 investment = True
             else:
-                print(f'[activeTrader]: Not investing in {symbol}; estimated change is {prediction[1]}; current price is {cur_price}.')
+                print('[activeTrader]: Not investing in {}; estimated change is {}%; current price is {}.'.format(symbol, prediction[1], cur_price))
         #should i sell?
         elif investment is True:
-            if prediction[1] < .0115:
-                print(f'[activeTrader]: Selling {symbol}; estimated change is {prediction[1]}; exit price is {cur_price}.')
+            if prediction[1] < 0.0115:       #0.0115
+                print('[activeTrader]: Selling {}; estimated change is {}%; exit price is {}.'.format(symbol, prediction[1], cur_price))
                 with open('trade_log.txt', 'a') as w:
-                    w.write(f'Sold at {cur_price}. Predicitons: {prediction};\n')
+                    w.write('Sold at {}. Original investment: {};\n'.format(cur_price, investPrice))
                 if notify is True:
-                    alert(f'{symbol}: Sold at {cur_price}. Predicitons: {prediction};')
+                    alert('{}: Sold at {}/original investment: {}; Predicitons: {};'.format(symbol, cur_price, investPrice, prediction))
                 investment = False
             else:
-                print(f'[activeTrader]: Estimated gain for {symbol} is {prediction[1]}; keeping investment; current price is {cur_price}.')
+                print('[activeTrader]: Keeping investment; estimated gain for {} is {}%; current price is {};'.format(symbol, prediction[1], cur_price))
 
 
 def main():
-    action = input(f'1. Get top {num_of_stocks} stocks\n2. Get stock data\n3. Train and run neural network prediction\n4. Run autonomous trade bot\n? ')
+    action = input('1. Get top {} stocks\n2. Get stock data\n3. Train and run neural network prediction\n4. Run autonomous trade bot\n? '.format(num_of_stocks))
+    if action.lower() == 'clean':
+        clean()
     if action == '1':
         getTopStocks()
-        main()
     elif action == '2':
         getStockData('All')
-        main()
     elif action == '3':
         symbol = input(':Run neural network on what stock? Type in symbol or leave blank to run stock_list.\n? ').upper()
         if symbol == '':
@@ -271,24 +280,22 @@ def main():
             stockPredictions.append([symbol, results[0], results[1], results[2]])
         preferredStock = ['None', 0]
         for resultSet in stockPredictions:
+            print('[Debug]: {} > {}'.format(resultSet[2], preferredStock[1]))
             if resultSet[2] > preferredStock[1]:
                 preferredStock[0] = resultSet[0]
                 preferredStock[1] = resultSet[2]
         if preferredStock[0] != 'None':
-            print(f':Neural Network preferred stock: {preferredStock[0]}; Estimated Gain: {preferredStock[1]}')
-            print(f': Starting Active Trader with preferred stock {[preferredStock[0]]}...')
+            print(':Neural Network preferred stock: {}; Estimated Gain: {}'.format(preferredStock[0]), preferredStock[1])
+            print(': Starting Active Trader with preferred stock {}...'.format([preferredStock[0]]))
             stock_list.remove(preferredStock[0])
-            for stock in stock_list:
-                try:
-                    os.remove(f'{stock}.xlsx')
-                except:
-                    pass
+            clean()
             activeTrader(preferredStock[0])
         else:
-            print(f':Neural Network did not determine preferred stock... Run again.')
-            main()
+            print(':Neural Network did not determine preferred stock... Run again.')
+    main()
 
 
 
 if __name__ == '__main__':
     main()
+
